fe615d39c4e13f74b0fbc5ae02706209e70a7843
lokesh.k.anand@gmail.com <lokesh.k.anand@gmail.com>
2018-02-01 20:27:32 -0600

    user interface built

---
+++ assets/javascript/rps.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyBANG8TYyd144m-1Z06lEw7CG36lr5f3kU",
+    authDomain: "rock-paper-scissors-9c357.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-9c357.firebaseio.com",
+    projectId: "rock-paper-scissors-9c357",
+    storageBucket: "rock-paper-scissors-9c357.appspot.com",
+    messagingSenderId: "961582757790"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
